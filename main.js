25ba4837226c6bfcbc51ca8b9d472040aad1e495
Yusuf Ahmed <108082685+Yusuf-4hmed@users.noreply.github.com>
2025-01-13 17:29:53 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,175 @@
+const startBtn = document.getElementById("start-btn");
+const nostartBtn = document.getElementById("nostart-btn");
+const nextBtn = document.getElementById("next-btn");
+
+
+// CARDS
+const quizInfoCard = document.getElementById("quiz-info-card");
+const quizCard = document.getElementById("quiz-card");
+const resultsCard = document.getElementById("results-card");
+// OPTIONS
+const options = document.getElementsByClassName("option");
+
+const optionOne = document.querySelector(".options div:nth-child(1)");
+const optionTwo = document.querySelector(".options div:nth-child(2)");
+const optionThree = document.querySelector(".options div:nth-child(3)");
+
+const questionHeader = document.getElementById("question-header");
+
+// MAIN INITIAL START BUTTON
+const start = () => {
+    quizInfoCard.classList.add("active");
+}
+// NON NO START
+const noStart = () => {
+    quizInfoCard.classList.remove("active");
+}
+
+let questions = [
+    {
+        number: 1,
+        question: "What's your ideal weekend activity?",
+        options: [
+            {text: "A. Staying home and watching a movie", type: "calm"},
+            {text: "B. Going out to eat, exploring or going for a walk", type: "energetic"},
+            {text: "C. Spending time with friends and family", type: "affectionate"}
+        ]
+    },
+    {
+        number: 2,
+        question: "How do you prefer to spend your weekday evenings?",
+        options: [
+            {text: "A. Relaxing with a good meal and some quiet time", type: "calm"},
+            {text: "B. Going to the gym or doing something active", type: "energetic"},
+            {text: "C. Meeting new people", type: "affectionate"}
+        ]
+    },
+    {
+        number: 3,
+        question: "How do you handle new situations or challenges?",
+        options: [
+            {text: "A. I take a step back and assess things carefully", type: "calm"},
+            {text: "B. I jump right in and tackle it head-on", type: "energetic"},
+            {text: "C. I seek advice from friends and make a collaborative decision", type: "affectionate"}
+        ]
+    },
+]
+
+let score = {
+    calm: 0,
+    energetic: 0,
+    affectionate: 0
+}
+
+let match = [
+    {
+        breed: "Birman",
+        description: "BMN Desc..",
+        why: "you were matched with BMN because..",
+        type: "calm"
+    },
+    {
+        breed: "Orange Tabby",
+        description: "ORNG TAB Desc..",
+        why: "you were matched with ORNG TAB because..",
+        type: "energetic"
+    },
+    {
+        breed: "British Shorthair",
+        description: "BR SH Desc..",
+        why: "you were matched with BR SH because..",
+        type: "affectionate"
+    }
+]
+
+let question = 0;
+
+// LET THE QUIZ BEGIN HEHE
+const enterQuiz = () => {
+    quizInfoCard.classList.remove("active");
+    quizCard.classList.add("active");
+    console.log(question);
+    // SHOWS FIRST QUESTIONS IN OPTION
+    questionHeader.innerHTML = `${questions[question].question}`;
+    optionOne.innerHTML = `${questions[question].options[0].text}`;
+    optionTwo.innerHTML = `${questions[question].options[1].text}`;
+    optionThree.innerHTML = `${questions[question].options[2].text}`;
+}
+
+// VISUAL FEEDBACK FOR SELECTION ON OPTION
+
+optionOne.addEventListener("click", () => {
+    optionOne.classList.add("selected");
+    optionTwo.classList.remove("selected");
+    optionThree.classList.remove("selected");
+})
+optionTwo.addEventListener("click", () => {
+    optionTwo.classList.add("selected");
+    optionOne.classList.remove("selected");
+    optionThree.classList.remove("selected");
+})
+optionThree.addEventListener("click", () => {
+    optionThree.classList.add("selected");
+    optionTwo.classList.remove("selected");
+    optionOne.classList.remove("selected");
+})
+
+const saveScore = () => {
+    // WILL SAVE SCORE
+        if (optionOne.classList.contains("selected")){
+            score.calm++;
+            console.log(score);
+        } else if (optionTwo.classList.contains("selected")){
+            score.energetic++;
+            console.log(score);
+        } else if (optionThree.classList.contains("selected")){
+            score.affectionate++;
+            console.log(score);
+        }
+}
+
+// ALLOWS NEXT BUTTON IF OPTION IS SELECTED
+ 
+optionOne.addEventListener("click", () => {
+    nextBtn.classList.remove("disabled");
+})
+optionTwo.addEventListener("click", () => {
+    nextBtn.classList.remove("disabled");
+})
+optionThree.addEventListener("click", () => {
+    nextBtn.classList.remove("disabled");
+})
+
+// NEXT QUESTION
+
+const nextQuestion = () => { 
+    saveScore();
+    if (question < 2){    
+        question++;
+        optionOne.classList.remove("selected");
+        optionTwo.classList.remove("selected");
+        optionThree.classList.remove("selected");
+        nextBtn.classList.add("disabled");
+        questionHeader.innerHTML = `${questions[question].question}`;
+        optionOne.innerHTML = `${questions[question].options[0].text}`;
+        optionTwo.innerHTML = `${questions[question].options[1].text}`;
+        optionThree.innerHTML = `${questions[question].options[2].text}`;
+    } else {
+        resultsCard.classList.add("active");
+        if (score.calm > score.energetic && score.affectionate){
+            alert(`The perfect cat for you is ${match[0].breed}`)
+        } else if (score.energetic > score.calm && score.affectionate) {
+            alert(`The perfect cat for you is ${match[1].breed}`)
+        } else if (score.affectionate > score.calm && score.energetic){
+            alert(`The perfect cat for you is ${match[2].breed}`)
+        }
+    }
+}
+
+// RESULTS CARD
+resultsCard.classList.contains("active")
+
+if (score.calm > score.energetic && score.affectionate){
+    alert(`The perfect cat for you is ${match.breed}`)
+}
+
